c03129997bb67762e90d6d82f107e0b35afde5d0
robbmj <michael.robb.prog@gmail.com>
2022-09-21 11:42:48 -0400

    added commands

--- commands.sh
+++ commands.sh
@@ -1,1 +1,40 @@
-# this is a comment to test git commit, and push
+# update and upgrade
+sudo pacman -Syu
+
+# Get git
+sudo pacman -S --needed base-devel
+
+# check version
+git --version
+
+# download and install rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# logout and log back in to update path
+logout
+
+# test rust install
+cargo --version
+rustc --version
+rustup --version
+
+mkdir -p ~/projects/rust
+
+# install paru
+cd ~/projects/rust
+sudo pacman -S --needed base-devel
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si
+paru --version
+cd ~
+
+# install alacritty (package 1 only)
+paru alacritty
+
+# download and install Firefox
+sudo pacman -S firefox 
+
+
+
+
